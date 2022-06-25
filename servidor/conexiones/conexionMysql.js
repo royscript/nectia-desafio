b761f7f0107dd598a0ce9e105cdedfc0cca39b82
royscript <roystandenb@gmail.com>
2022-06-25 01:08:01 -0400

    cambios en script sql y conexionMysql

--- servidor/conexiones/conexionMysql.js
+++ servidor/conexiones/conexionMysql.js
@@ -6,7 +6,7 @@ class conexionMysql {
         this.conexion = mysql.createPool({
             host : process.env.HOST_MYSQL,
             user : process.env.USER_MYSQL,
-            //password : process.env.PASSWORD_MYSQL,
+            password : process.env.PASSWORD_MYSQL,
             database : process.env.DATABASE_MYSQL
         });
     }
